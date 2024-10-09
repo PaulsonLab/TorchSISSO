88f3ce05d8631ed6cc17f4db2bb928222a592dc6
Madhav <116205116+MR1319@users.noreply.github.com>
2024-10-09 09:18:09 -0400

    Update __init__.py

--- src/TorchSisso/__init__.py
+++ src/TorchSisso/__init__.py
@@ -14,4 +14,4 @@ from .Regressor_dimension import Regressor
 
 from .DimensionalFeatureSpaceConstruction import feature_space_construction
 
-from .model import sisso_model
+from .model import SissoModel
